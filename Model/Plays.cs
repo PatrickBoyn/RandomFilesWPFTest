31cbb3ea9bb105b64722ab0df5d086723d588716
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-12-02 20:51:19 -0500

    Added a prop.

--- Model/Plays.cs
+++ Model/Plays.cs
@@ -5,6 +5,7 @@ namespace RandomFilesTest.Model
     public class Plays
     {
         public int Id { get; set; }
+        public string VideoName { get; set; }
         public DateTime LastPlayed { get; set; }
         public int NumberOfPlays { get; set; }
     }
