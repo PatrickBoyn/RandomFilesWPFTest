4ff37bad823778ee564654e9a4c634a510404061
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-12-02 20:58:11 -0500

    Added props for the videos model.

--- Model/Videos.cs
+++ Model/Videos.cs
@@ -1,7 +1,12 @@
-﻿namespace RandomFilesTest.Model
+﻿using System;
+
+namespace RandomFilesTest.Model
 {
     public class Videos
     {
-        
+        public int Id { get; set; }
+        public string VideoName { get; set; }
+        public string VideoPath { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
