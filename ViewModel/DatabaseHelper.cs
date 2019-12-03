276eb780df70c1cca4edc21ab20f2bbf54972af7
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-12-03 13:59:15 -0500

    Added CRUD functionality, may not use all of it.

--- ViewModel/DatabaseHelper.cs
+++ ViewModel/DatabaseHelper.cs
@@ -1,7 +1,60 @@
-﻿namespace RandomFilesTest.ViewModel
+﻿using System;
+using System.IO;
+using SQLite;
+
+namespace RandomFilesTest.ViewModel
 {
     public class DatabaseHelper
     {
-        
+        private static string dbFile = Path.Combine(Environment.CurrentDirectory, "RandomFiles.db3");
+
+        public static bool Insert<T>(T item)
+        {
+            bool result = false;
+            
+            using (var connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                int numberOfRows = connection.Insert(item);
+                if (numberOfRows > 0)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public static bool Update<T>(T item)
+        {
+            bool result = false;
+            using (var connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                int numberOfRows = connection.Update(item);
+                
+                if (numberOfRows > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public static bool Delete<T>(T item)
+        {
+            bool result = false;
+            using (var connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                int numberOfRows = connection.Delete<T>(item);
+                if (numberOfRows > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
     }
 }
